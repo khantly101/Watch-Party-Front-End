c120bac6214b6a1d5eb32e1932012b6eca9949d2
khantly101 <khantuanly@gmail.com>
2019-06-29 14:09:47 -0700

    moved create users to its own component

--- src/App.js
+++ src/App.js
@@ -1,62 +1,16 @@
 import React from 'react'
-import './App.css'
+import NewUser from './components/newUser.js'
 
-let baseURL = 'http://localhost:3003' 
+import './App.css'
 
 class App extends React.Component {
 	state = {
-		user: [],
-		userName: '',
-		password: ''
-	}
 
-	handleChange = (event) => {
-		this.setState({ [event.target.id]: event.target.value})
-	}
-
-	handleAddUser = (user) => {
-		const copyUser = [...this.state.user]
-		copyUser.unshift(user)
-		this.setState({
-			user: copyUser,
-			userName: '',
-			password: ''
-		})
-	}
-
-	handleSubmit = (event) => {
-		event.preventDefault()
-		fetch(baseURL + '/new', {
-			method: 'POST',
-			body: JSON.stringify(
-				{
-					userName: this.state.userName,
-					password: this.state.password
-				}),
-			headers: {
-				'Content-Type': 'application/json'
-			}
-		}).then (res => res.json())
-		.then (resJson => {
-			this.handleAddUser(resJson)
-			this.setState({
-				userName: '',
-				password: ''
-			})
-		}).catch (error => console.error({'Error': error}))
 	}
 
 	render () {
 		return (
-			<div>
-				<form onSubmit={this.handleSubmit}>
-					<label htmlFor="userName"></label>
-					<input type="text" id="userName" name="userName" onChange={this.handleChange} value={this.state.userName} placeholder="userName"/>
-					<label htmlFor="password"></label>
-					<input type="password" id="password" name="password" onChange={this.handleChange} value={this.state.password} placeholder="password"/>
-					<input type="submit" value="New User"/>
-				</form>
-			</div>
+			<NewUser />
 		)
 	}
 }
---
+++ src/components/newUser.js
@@ -0,0 +1,63 @@
+import React from 'react'
+
+let baseURL = 'http://localhost:3003' 
+
+class NewUser extends React.Component {
+	state = {
+		user: [],
+		userName: '',
+		password: ''
+	}
+
+	handleChange = (event) => {
+		this.setState({ [event.target.id]: event.target.value})
+	}
+
+	handleAddUser = (user) => {
+		const copyUser = [...this.state.user]
+		copyUser.unshift(user)
+		this.setState({
+			user: copyUser,
+			userName: '',
+			password: ''
+		})
+	}
+
+	handleSubmit = (event) => {
+		event.preventDefault()
+		fetch(baseURL + '/new', {
+			method: 'POST',
+			body: JSON.stringify(
+				{
+					userName: this.state.userName,
+					password: this.state.password
+				}),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		}).then (res => res.json())
+		.then (resJson => {
+			this.handleAddUser(resJson)
+			this.setState({
+				userName: '',
+				password: ''
+			})
+		}).catch (error => console.error({'Error': error}))
+	}
+
+	render () {
+		return (
+			<div>
+				<form onSubmit={this.handleSubmit}>
+					<label htmlFor="userName"></label>
+					<input type="text" id="userName" name="userName" onChange={this.handleChange} value={this.state.userName} placeholder="userName"/>
+					<label htmlFor="password"></label>
+					<input type="password" id="password" name="password" onChange={this.handleChange} value={this.state.password} placeholder="password"/>
+					<input type="submit" value="New User"/>
+				</form>
+			</div>
+		)
+	}
+}
+
+export default NewUser
