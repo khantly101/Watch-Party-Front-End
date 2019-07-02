8ad221ebb6fa780334fb80c73fb078dff439d8b7
khantly101 <khantuanly@gmail.com>
2019-07-01 22:17:45 -0700

    forgot partyrooms

--- src/components/profile.js
+++ src/components/profile.js
@@ -9,6 +9,7 @@ class Profile extends React.Component {
 				<h1>{this.props.state.lastName}</h1>
 				<h1>{this.props.state.img}</h1>
 				<h1>{this.props.state.info}</h1>
+				<h1>{this.props.state.partyrooms}</h1>
 			</div>
 		)
 	}
