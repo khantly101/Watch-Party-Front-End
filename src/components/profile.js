90c6438e58f49ffc2e0de1bc93a48e7e1afd3aaa
khantly101 <khantuanly@gmail.com>
2019-07-04 14:26:34 -0700

    added css to profiles

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,5 +1,5 @@
 import React 		from 'react'
-// import { Link } 	from 'react-router-dom'
+import { Link } 	from 'react-router-dom'
 import Default 		from './Images/default.svg'
 
 let baseURL = 'http://localhost:3003' 
@@ -39,16 +39,37 @@ class Profile extends React.Component {
 	render () {
 		return (
 			<div className='container-fluid'>
-				<div className='border rounded'>
-					<div className='jumbotron bg-white'>
-						<div className='float-right'>
-							{
-								this.props.state.img ? <img width='150px' className='img-thumbnail rounded' src={this.props.state.img} alt='Missing' /> : <img width='150px' className='img-thumbnail rounded' src={Default} alt='Missing' />
-							}
-						</div>
-						<br />
-						<h1 className='display-4'>Hello, {this.props.state.firstName} {this.props.state.lastName}</h1>
-						<p className='display-12'>UserName: {this.props.state.currentUser}</p>
+				<div className='jumbotron bg-white'>
+					<div className='float-right'>
+						{
+							this.props.state.img ? <img width='150px' className='img-thumbnail rounded' src={this.props.state.img} alt='Missing' /> : <img width='150px' className='img-thumbnail rounded' src={Default} alt='Missing' />
+						}
+					</div>
+					<br />
+					<h1 className='display-4'>Hello, {this.props.state.firstName} {this.props.state.lastName}</h1>
+					<p className='display-12'>UserName: {this.props.state.currentUser}</p>
+					<button>Edit Profile</button>
+					<br />
+					<hr />
+					<p>About Me:</p>
+					<p>{this.props.state.info}</p>
+				</div>
+				<br />
+				<div>
+					<Link to='/NewRoom'><button className="btn btn-primary btn-lg btn-block">Create Party Room</button></Link>
+					<div className="table-responsive">
+						<table className="table table-light">
+							<thead>
+								<tr>
+									<th scope="col">#</th>
+									<th scope="col">Room Name</th>
+									<th scope="col">Description</th>
+									<th scope="col">Visit</th>
+									<th scope="col">Edit</th>
+									<th scope="col">Delete</th>
+								</tr>
+							</thead>
+						</table>
 					</div>
 				</div>
 			</div>
@@ -62,7 +83,6 @@ export default Profile
 
 			// <div className='container-fluid'>
 			// 	<div>
-			// 		<h1>{this.props.state.info}</h1>
 			// 		<h1>{this.props.state.partyrooms}</h1>
 			// 	</div>
 			// 	<div>
