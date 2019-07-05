57e9f5c49e0ecd7275ca2dd5d7a529f088124321
Luis Rosario <luisrosariomix@gmail.com>
2019-07-05 10:50:38 -0700

    Fixed socket connections... Users are now creating new socket connections. Added chat room list.

--- src/components/ChatRoom.js
+++ src/components/ChatRoom.js
@@ -2,8 +2,6 @@ import React         from 'react'
 import ReactJWPlayer from 'react-jw-player'
 import io	           from 'socket.io-client'
 
-const socket = io('http://localhost:3003')
-
 class ChatRoom extends React.Component {
 	state = {
 		partyRooms:[],
@@ -15,7 +13,8 @@ class ChatRoom extends React.Component {
 		file: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4',
 		playerId: '',
 		clients: [],
-		clientId: ''
+		clientId: '',
+		socket: io('http://localhost:3003')
 	}
 
 	componentWillMount() {
@@ -29,41 +28,42 @@ class ChatRoom extends React.Component {
 		}, () => {
 			console.log(this.state.partyRooms)
 			console.log(this.state.partyRoomIndex)
+
 		})
 	}
 
-	socketConnect = (theRoom,userName,pic) => {
-			socket.on('connect', function() {
+	socketConnect = (theRoom, roomIndex, userName, pic, socket) => {
+			this.state.socket.on('connect', function() {
 				console.log(`Connection made`)
 				console.log(`On Connection ` + theRoom)
 				console.log(`On Connection ` + userName)
 				console.log(`On Connection ` + pic)
 				console.log(socket.id + ' ' + socket.disconnected)
-				socket.emit('clientData',theRoom,userName,socket.id )
+				socket.emit('clientData',theRoom,roomIndex,userName,pic,socket.id )
 
 				// Connected, let's sign-up for to receive messages for this room
-				socket.emit('room', theRoom,userName,pic,socket.id)
+				socket.emit('room', theRoom, roomIndex, userName, pic, socket.id)
+				console.log(`current clients`)
+				console.log(theRoom.clients)
 			})
 
 		}
 
 	componentDidMount() {
 
-		socket.on(`setId`, (msg,id) => {
-			let newObject = {
-				sockId: id,
-				playerId: 'partyVideo-' + id
-			}
+		this.socketConnect(this.state.partyRooms[this.state.partyRoomIndex], this.state.partyRoomIndex, this.state.userName, this.state.pic,this.state.socket)
+
+		this.state.socket.on(`addToList`, (msg, partyRoom, roomIndex, userName, pic, id) => {
+
+			let partyRooms = [...this.state.partyRooms]
+			partyRooms[roomIndex].clients = partyRoom.clients
 			this.setState({
-				clients: [...this.state.clients, newObject],
-				clientId: id
-				// playerId: 'partyVideo'
-				}, () => {
-					// console.log(newObject)
-				})
+				partyRooms:[...this.state.partyRooms]
+			})
 		})
+
 		//Listening to responses sent from server
-		socket.on(`recieveMessage`, (msg,pic,userName) => {
+		this.state.socket.on(`recieveMessage`, (msg,pic,userName) => {
 				 //Looking to see if we get responses back from server
 				 // console.log(msg)
 				 let newObject = {
@@ -80,22 +80,25 @@ class ChatRoom extends React.Component {
 				 })
 			 })
 
-		socket.on(`play`, (msg,playerId) => {
+		this.state.socket.on(`play`, (msg,playerId) => {
 			// console.log('Triggering ' +	msg)
 			// console.log('Triggering ' + playerId)
 			window.jwplayer().play()
 		})
 
-		socket.on(`stop`, (msg,playerId) => {
+		this.state.socket.on(`stop`, (msg,playerId) => {
 	 	 	// console.log('Triggering ' +	msg)
 	 		// console.log('Triggering ' + playerId)
 	 		window.jwplayer().stop()
  		})
 
- 		socket.on(`delete`, (msg,clientId) => {
+ 		this.state.socket.on(`delete`, (msg,clientId) => {
 	 		// console.log(clientId + ' ' + msg)
 
 		})
+	}
+	componentWillUnmount() {
+		this.state.socket.close()
 	}
 
 	handleChange = (event) => {
@@ -104,7 +107,7 @@ class ChatRoom extends React.Component {
 
 	handleSubmit = (event) => {
 		event.preventDefault()
-		socket.emit(`sendMessage`, this.state.chatMessage,this.state.partyRooms[this.state.partyRoomIndex]._id,this.state.pic,this.state.userName)
+		this.state.socket.emit(`sendMessage`, this.state.chatMessage,this.state.partyRooms[this.state.partyRoomIndex]._id,this.state.pic,this.state.userName)
 		this.setState({
 			chatMessage: ''
 		})
@@ -112,18 +115,19 @@ class ChatRoom extends React.Component {
 
 	sendPlay = (playerId) => {
 		// console.log(`Sending ` + playerId)
-		socket.emit(`play`, `sendPlay` ,this.state.partyRooms[this.state.partyRoomIndex]._id,playerId)
+		console.log(this.state.partyRooms[this.state.partyRoomIndex]._id)
+		this.state.socket.emit(`play`, `sendPlay` ,this.state.partyRooms[this.state.partyRoomIndex]._id,playerId)
 	}
 
 	sendStop = (playerId) => {
 		// console.log(`Sending ` + playerId)
-		socket.emit(`stop`, `sendStop` ,this.state.partyRooms[this.state.partyRoomIndex]._id,playerId)
+		this.state.socket.emit(`stop`, `sendStop` ,this.state.partyRooms[this.state.partyRoomIndex]._id,playerId)
 	}
 
 	render() {
+
 		return (
 			<React.Fragment>
-				{this.socketConnect(this.state.partyRooms[this.state.partyRoomIndex]._id,this.state.userName, this.state.pic)}
 
 				<button onClick={ () => { this.sendPlay() } }>Play Video</button>
 			 	<button onClick={ () => { this.sendStop() } }>Stop Video</button>
@@ -135,9 +139,21 @@ class ChatRoom extends React.Component {
 				/>
 				<div>
 
+				{this.state.partyRooms[this.state.partyRoomIndex].clients.map((theClient, index) => {
+					return (
+						<div key={index}>
+						<h1>This is current User List in Chat Room </h1>
+						imagePlaceholder: {theClient.pic} username: {theClient.userName} socketId: {theClient.sockId}
+
+						</div>
+					)
+				})}
+
+
 				{this.state.partyRooms[this.state.partyRoomIndex].messages.map((theMessage, index) => {
 					return (
 						<div key={index}>
+						<h1>This is current message board for chat room </h1>
 						imagePlaceholder: {theMessage.pic} username: {theMessage.userName} message: {theMessage.message}
 
 						</div>
